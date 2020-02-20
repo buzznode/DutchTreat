56d3b2aca1e2972d31566a541b1733d97689a3c9
BRAD-DESKTOP\bradd <brad.duderstadt@gmail.com>
2020-02-19 21:58:01 -0600

    Added some javascript, jquery and additional styles

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -19,6 +19,7 @@ namespace DutchTreat
 		{
 			app.UseDefaultFiles();
 			app.UseStaticFiles();
+			app.UseNodeModules();
 		}
 	}
 }
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,12 @@
+﻿/* Hide the form */
+$('#theForm').hide();
+
+/* Handle btnBuy click */
+$('#btnBuy').on('click', function () {
+  console.log('Buying Item');
+});
+
+/* Handle product properties clicks */
+$('.product-props li').on('click', function () {
+  console.log('You clicked on ' + $(this).text());
+});
